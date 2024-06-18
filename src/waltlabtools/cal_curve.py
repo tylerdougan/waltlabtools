Fixed 4PL and 5PL model Jacobians, simplified dropna, and improved CalCurve.

src/waltlabtools/cal_curve.py
@@ -14,8 +14,10 @@ from sklearn.exceptions import DataConversionWarning
 from sklearn.utils._param_validation import Interval, StrOptions
 from sklearn.utils.validation import (
     _check_sample_weight,  # type: ignore
+    check_array,
     check_is_fitted,
     check_non_negative,
+    column_or_1d,
 )
 
 from ._backend import gmean, np
@@ -168,6 +170,7 @@ class CalCurve(BaseEstimator, RegressorMixin, TransformerMixin):
         "lod_sds": [Interval(Real, 0, None, closed="left")],
         "max_iter": [Interval(Integral, 0, None, closed="left"), None],
         "ensure_2d": ["boolean"],
+        "sample_weight": [StrOptions(set(_WEIGHTING_SCHEMES)), None],
     }
 
     def __init__(
@@ -180,6 +183,7 @@ class CalCurve(BaseEstimator, RegressorMixin, TransformerMixin):
         lod_sds: float = 3,
         max_iter: Optional[int] = None,
         ensure_2d: bool = False,
+        sample_weight: Optional[str] = "1/y",
         **kwargs,
     ):
         self.model = model
@@ -190,6 +194,7 @@ class CalCurve(BaseEstimator, RegressorMixin, TransformerMixin):
         self.lod_sds = lod_sds
         self.max_iter = max_iter
         self.ensure_2d = ensure_2d
+        self.sample_weight = sample_weight
 
         if kwargs:
             warnings.warn(
@@ -199,27 +204,12 @@ class CalCurve(BaseEstimator, RegressorMixin, TransformerMixin):
                 DeprecationWarning,
             )
 
-    def _validate_data(
-        self,
-        X="no_validation",
-        y="no_validation",
-        reset=True,
-        validate_separately=False,
-        cast_to_ndarray=True,
-        **check_params,
-    ):
-        check_params = {
-            "ensure_2d": self.ensure_2d,
-            "force_all_finite": False,
-        } | check_params
-        return super()._validate_data(  # type: ignore
-            X=X,
-            y=y,
-            reset=reset,
-            validate_separately=validate_separately,
-            cast_to_ndarray=cast_to_ndarray,
-            **check_params,
-        )
+    def _get_sample_weight(self, X, y, sample_weight=None):
+        if sample_weight is None:
+            sample_weight = self.sample_weight
+        if isinstance(sample_weight, str) and sample_weight in _WEIGHTING_SCHEMES:
+            sample_weight = _WEIGHTING_SCHEMES[sample_weight](X, y)
+        return column_or_1d(sample_weight, warn=True)
 
     def _aggregate_replicates(
         self, X: np.ndarray, y: np.ndarray, sample_weight: np.ndarray
@@ -242,10 +232,36 @@ class CalCurve(BaseEstimator, RegressorMixin, TransformerMixin):
 
         return unique_X, unique_y, unique_weight, blank
 
-    def _get_sample_weight(self, X, y, sample_weight=None):
-        if isinstance(sample_weight, str) and sample_weight in _WEIGHTING_SCHEMES:
-            sample_weight = _WEIGHTING_SCHEMES[sample_weight](X, y)
-        return _check_sample_weight(sample_weight, X)
+    def _preprocess_fit_data(self, X, y, sample_weight=None):
+        # check X, y
+        X = self._validate_data(
+            X=X,
+            force_all_finite=False,
+            ensure_2d=self.ensure_2d,
+            ensure_min_samples=len(self._model.coef_init),
+        )
+        y = check_array(
+            y, force_all_finite=False, ensure_2d=False, estimator=self, input_name="y"
+        )
+        if np.ndim(X) > 1:
+            if self.n_features_in_ > 1 and not self.ensure_2d:  # type: ignore
+                warnings.warn(
+                    "CalCurve input X should be a 1d array or 2d array with 1 feature; "
+                    f"using only the first of {self.n_features_in_} features.",  # type: ignore
+                    DataConversionWarning,
+                )
+            X = X[:, 0]
+
+        # get sample weight
+        sample_weight = self._get_sample_weight(X, y, sample_weight)
+
+        # drop NANs and aggregate replicates
+        X, y, sample_weight = dropna(X, y, sample_weight)
+        self.X_, self.y_ = self._validate_data(X, y, ensure_2d=False)
+        if self.ensure_2d:
+            check_non_negative(self.X_, whom=self.__class__.__name__)
+        sample_weight = _check_sample_weight(sample_weight, self.X_)
+        return self._aggregate_replicates(self.X_, self.y_, sample_weight)
 
     def fit(self, X, y, sample_weight=None):
         """Fit the model to data.
@@ -271,28 +287,7 @@ class CalCurve(BaseEstimator, RegressorMixin, TransformerMixin):
         self._model = _get_value_or_key(MODELS, self.model)
 
         # data
-        X, y = self._validate_data(  # type: ignore
-            X,
-            y,
-            ensure_min_samples=len(self._model.coef_init),
-            y_numeric=True,
-        )
-        if self.ensure_2d:
-            check_non_negative(X, whom=self.__class__.__name__)
-        if np.ndim(X) > 1:
-            if self.n_features_in_ > 1 and not self.ensure_2d:  # type: ignore
-                warnings.warn(
-                    "CalCurve input X should be a 1d array or 2d array with 1 feature; "
-                    f"using only the first of {self.n_features_in_} features.",  # type: ignore
-                    DataConversionWarning,
-                )
-            X = X[:, 0]
-        self.X_, self.y_, sample_weight = dropna(
-            X, y, self._get_sample_weight(X, y, sample_weight)
-        )
-        X, y, sample_weight, blank = self._aggregate_replicates(
-            self.X_, self.y_, sample_weight
-        )
+        X, y, sample_weight, blank = self._preprocess_fit_data(X, y, sample_weight)
 
         # coef_init
         if self.warm_start and hasattr(self, "coef_"):
@@ -303,23 +298,20 @@ class CalCurve(BaseEstimator, RegressorMixin, TransformerMixin):
             coef_init = self.coef_init
 
         # optimization
-        def loss_func(coefs):
+        def loss_func(coefs, *, X, sample_weight):
             predictions = self._model.func(X, *coefs)
             residuals = predictions - y
             return residuals * sample_weight
 
-        if self._model.jac is not None:
-            jac = partial(self._model.jac, X=X, y=y, sample_weight=sample_weight)
-        else:
-            jac = "2-point"
         with warnings.catch_warnings():
             warnings.simplefilter("ignore")
             result = least_squares(
                 loss_func,
                 x0=coef_init,
-                jac=jac,  # type: ignore
+                jac=self._model.jac,
                 max_nfev=self.max_iter,
                 method=self.solver,
+                kwargs={"X": X, "sample_weight": sample_weight},
             )
         self.coef_ = result.x
         self.n_iter_ = result.nfev
@@ -371,7 +363,9 @@ class CalCurve(BaseEstimator, RegressorMixin, TransformerMixin):
             Predicted signal values.
         """
         check_is_fitted(self)
-        X = self._validate_data(X=X, reset=False)
+        X = self._validate_data(
+            X=X, reset=False, force_all_finite=False, ensure_2d=self.ensure_2d
+        )
         if np.ndim(X) == 1:
             return self.signal(X)
         else:
@@ -391,12 +385,14 @@ class CalCurve(BaseEstimator, RegressorMixin, TransformerMixin):
             Transformed signal values.
         """
         check_is_fitted(self)
-        X = self._validate_data(X=X, reset=False)
+        X = self._validate_data(
+            X=X, reset=False, force_all_finite=False, ensure_2d=self.ensure_2d
+        )
         # if self.ensure_2d:
         #     X = X[:, 0].reshape(-1, 1)
         return self.signal(X)
 
-    def inverse_transform(self, y):
+    def inverse_transform(self, X):
         """Estimate the concentration for given signal values.
 
         Parameters
@@ -411,8 +407,10 @@ class CalCurve(BaseEstimator, RegressorMixin, TransformerMixin):
         """
         # (n_samples, n_features) -> (n_samples, n_features)
         check_is_fitted(self)
-        y = self._validate_data(y=y, reset=False, y_numeric=True)
-        return self.conc(y)
+        X = self._validate_data(
+            X=X, reset=False, force_all_finite=False, ensure_2d=self.ensure_2d
+        )
+        return self.conc(X)
 
     def _make_curve_points(self, **kwargs):
         """Make the points to interpolate for plotting a calibration curve.
@@ -512,7 +510,7 @@ class CalCurve(BaseEstimator, RegressorMixin, TransformerMixin):
             "label": f"limit of detection: {self.lod_:.2g}",
         } | match_kwargs("lod_", kwargs)
         if np.isfinite(lod_kwargs["x"]):
-            ax.axvline(**lod_kwargs)
+            ax.axvline(lod_kwargs.pop("x"), **lod_kwargs)
 
         curve_kwargs = {
             "x": X_curve,
@@ -520,7 +518,7 @@ class CalCurve(BaseEstimator, RegressorMixin, TransformerMixin):
             "color": "tab:green",
             "label": f"{self._model.name} calibration curve",
         } | match_kwargs("curve_", kwargs)
-        ax.plot(**curve_kwargs)
+        ax.plot(curve_kwargs.pop("x"), curve_kwargs.pop("y"), **curve_kwargs)
 
         point_kwargs = {
             "x": self.X_,
@@ -531,7 +529,7 @@ class CalCurve(BaseEstimator, RegressorMixin, TransformerMixin):
             "alpha": 0.5,
             "label": "calibrator points",
         } | match_kwargs("point_", kwargs)
-        ax.plot(**point_kwargs)
+        ax.plot(point_kwargs.pop("x"), point_kwargs.pop("y"), **point_kwargs)
 
         ax_kwargs = (
             {

src/waltlabtools/model.py
@@ -41,6 +41,20 @@ class Model:
         The natural scaling transformations for `x` and `y`. For
         example, "log" means that the data may be distributed
         log-normally and are best visualized on a log scale.
+    jac : {'2-point', '3-point', 'cs', callable}, optional
+        Method of computing the Jacobian matrix (an m-by-n matrix, where
+        element (i, j) is the partial derivative of f[i] with respect to
+        x[j]) of the loss function. The keywords select a finite
+        difference scheme for numerical estimation. The scheme '3-point'
+        is more accurate, but requires twice as many operations as
+        '2-point' (default). The scheme 'cs' uses complex steps, and
+        while potentially the most accurate, it is applicable only when
+        `func` correctly handles complex inputs and can be analytically
+        continued to the complex plane. If callable, it is used as
+        ``jac(x, *args, **kwargs)`` and should return a good approximation
+        (or the exact value) for the Jacobian as an array_like (np.atleast_2d
+        is applied), a sparse matrix (csr_matrix preferred for performance) or
+        a `scipy.sparse.linalg.LinearOperator`.
 
     """
 
@@ -53,7 +67,7 @@ class Model:
         plaintext_formula: str = "",
         xscale: str | ScaleBase = "linear",
         yscale: str | ScaleBase = "linear",
-        jac: Optional[Callable] = None,
+        jac: str | Callable = "2-point",
     ):
         self.func = func
         self.inverse = inverse
@@ -77,7 +91,9 @@ def linear_inverse(y, a: float = 1, b: float = 0):
     return (y - b) / a
 
 
-def jac_linear(coef, X, y, sample_weight):
+def jac_linear(
+    coef: np.ndarray, *, X: np.ndarray, sample_weight: np.ndarray
+) -> np.ndarray:
     J_a = X * sample_weight
     J_b = sample_weight
     return np.column_stack((J_a, J_b))
@@ -106,13 +122,13 @@ def power_inverse(y, a: float = 1, b: float = 1):
     return (y / b) ** (1 / a)
 
 
-def jac_power(coef, X: np.ndarray, y: np.ndarray, sample_weight: np.ndarray):
-    a, b = coef  # Unpack the coefficients
-    # Derivative with respect to a
-    J_a = b * X**a * np.log(X + EPS) * sample_weight
-    # Derivative with respect to b
+def jac_power(
+    coef: np.ndarray, *, X: np.ndarray, sample_weight: np.ndarray
+) -> np.ndarray:
+    a, b = coef
+    J_a = b * X**a * np.where(X > 0, np.log(X), 0) * sample_weight
     J_b = X**a * sample_weight
-    return np.column_stack((J_a, J_b))  # Shape (n_samples, 2)
+    return np.column_stack((J_a, J_b))
 
 
 power_model = Model(
@@ -136,16 +152,18 @@ def hill_inverse(y, a: float = 1, b: float = 1, c: float = 1):
     return c * (a / y - 1) ** (-1 / b)
 
 
-def jac_hill(coef, X, y, sample_weight):
-    a, b, c = coef  # Unpack the coefficients
-    denominator = c**b + X**b
-    # Derivative with respect to a
-    J_a = (X**b / (denominator)) * sample_weight
-    # Derivative with respect to b
-    J_b = a * (c * X) ** b * np.log((X + EPS) / c) / (denominator**2) * sample_weight
-    # Derivative with respect to c
-    J_c = (-a * X**b * b * c ** (b - 1) / (denominator**2)) * sample_weight
-    return np.column_stack((J_a, J_b, J_c))  # Shape (n_samples, 3)
+def jac_hill(
+    coef: np.ndarray, *, X: np.ndarray, sample_weight: np.ndarray
+) -> np.ndarray:
+    a, b, c = coef
+    X_b = X**b
+    c_b = c**b
+    denom = (c_b + X_b) ** 2
+
+    J_a = X_b / (c_b + X_b)
+    J_b = a * X_b * np.where(X > 0, np.log(X), 0) * (c_b - X_b) / denom
+    J_c = -a * X_b * b * c ** (b - 1) / denom
+    return np.column_stack((J_a, J_b, J_c)) * sample_weight[:, np.newaxis]
 
 
 hill_model = Model(
@@ -171,6 +189,21 @@ def logistic_inverse(y, a: float = 1, b: float = 1, c: float = 0, d: float = 0):
     return c - np.log((a - d) / (y - d) - 1) / b
 
 
+def jac_logistic(
+    coef: np.ndarray, *, X: np.ndarray, sample_weight: np.ndarray
+) -> np.ndarray:
+    a, b, c, d = coef
+    exp_term = np.exp(-b * (X - c))
+    denom = 1 + exp_term
+    denom2 = denom**2
+
+    J_a = 1 / denom
+    J_b = (a - d) * (X - c) * exp_term / denom2
+    J_c = (a - d) * b * exp_term / denom2
+    J_d = exp_term / denom
+    return np.column_stack((J_a, J_b, J_c, J_d)) * sample_weight[:, np.newaxis]
+
+
 logistic_model = Model(
     func=logistic,
     inverse=logistic_inverse,
@@ -179,6 +212,7 @@ logistic_model = Model(
     plaintext_formula="y = d + (a - d) / {1 + exp[-b (x - c)]}",
     xscale="linear",
     yscale="linear",
+    jac=jac_logistic,
 )
 
 
@@ -193,20 +227,17 @@ def three_param_logistic_inverse(y, a: float = 0, c: float = 1, d: float = 30):
     return c * ((a - d) / (y - d) - 1)
 
 
-def jac_three_param_logistic(coef, X, y, sample_weight):
+def jac_three_param_logistic(
+    coef: np.ndarray, *, X: np.ndarray, sample_weight: np.ndarray
+) -> np.ndarray:
     a, c, d = coef
-    one_plus_X_over_c = 1 + X / c
+    denom = 1 + X / c
 
-    # Derivative with respect to a
-    J_a = (1 / one_plus_X_over_c) * sample_weight
+    J_a = 1 / denom
+    J_c = (a - d) * X / (c**2 * denom**2)
+    J_d = X / (c + X)
 
-    # Derivative with respect to c
-    J_c = ((a - d) * X) / (c**2 * one_plus_X_over_c**2) * sample_weight
-
-    # Derivative with respect to d
-    J_d = (1 - 1 / one_plus_X_over_c) * sample_weight
-
-    return np.column_stack((J_a, J_c, J_d))
+    return np.column_stack((J_a, J_c, J_d)) * sample_weight[:, np.newaxis]
 
 
 three_param_logistic_model = Model(
@@ -234,26 +265,20 @@ def four_param_logistic_inverse(
     return c * ((a - d) / (y - d) - 1) ** (1 / b)
 
 
-def jac_four_param_logistic(coef, X, y, sample_weight):
+def jac_four_param_logistic(
+    coef: np.ndarray, *, X: np.ndarray, sample_weight: np.ndarray
+) -> np.ndarray:
     a, b, c, d = coef
-    X_over_c_b = (X / c) ** b
-    one_plus_X_over_c_b = 1 + X_over_c_b
+    X_div_c = X / c
+    X_div_c_b = X_div_c**b
+    denom = 1 + X_div_c_b
+    denom2 = denom**2
 
-    # Derivative with respect to a
-    J_a = (1 / one_plus_X_over_c_b) * sample_weight
-
-    # Derivative with respect to b
-    J_b = (
-        (a - d) * X_over_c_b * np.log((X + EPS) / c) / one_plus_X_over_c_b**2
-    ) * sample_weight
-
-    # Derivative with respect to c
-    J_c = ((a - d) * b * X**b / (c ** (b + 1) * one_plus_X_over_c_b**2)) * sample_weight
-
-    # Derivative with respect to d
-    J_d = (1 - 1 / one_plus_X_over_c_b) * sample_weight
-
-    return np.column_stack((J_a, J_b, J_c, J_d))
+    J_a = 1 / denom
+    J_b = -(a - d) * np.where(X > 0, X_div_c_b * np.log(X_div_c) / denom2, 0)
+    J_c = (a - d) * b * X**b / (c ** (b + 1) * denom2)
+    J_d = 1 - J_a
+    return np.column_stack((J_a, J_b, J_c, J_d)) * sample_weight[:, np.newaxis]
 
 
 four_param_logistic_model = Model(
@@ -283,32 +308,32 @@ def five_param_logistic_inverse(
     return c * (((a - d) / (y - d)) ** (1 / g) - 1) ** (1 / b)
 
 
-def jac_five_param_logistic(coef, X, y, sample_weight):
+def jac_five_param_logistic(
+    coef: np.ndarray, *, X: np.ndarray, sample_weight: np.ndarray
+) -> np.ndarray:
     a, b, c, d, g = coef
-    X_over_c_b = (X / c) ** b
-    one_plus_X_over_c_b = 1 + X_over_c_b
-    # Derivative with respect to a
-    J_a = (1 / one_plus_X_over_c_b**g) * sample_weight
-    # Derivative with respect to b
+    X_div_c = X / c
+    X_div_c_b = X_div_c**b
+    denom = (1 + X_div_c_b) ** g
+    denom_g_plus_1 = (1 + X_div_c_b) ** (g + 1)
+
+    J_a = 1 / denom
     J_b = (
-        g
-        * (a - d)
-        * X_over_c_b
-        * np.log((X + EPS) / c)
-        / one_plus_X_over_c_b ** (g + 1)
-    ) * sample_weight
-    # Derivative with respect to c
-    J_c = (
-        g * (a - d) * b * X**b / (c ** (b + 1) * one_plus_X_over_c_b ** (g + 1))
-    ) * sample_weight
-    # Derivative with respect to d
-    J_d = (1 - 1 / one_plus_X_over_c_b**g) * sample_weight
-    # Derivative with respect to g
-    J_g = (
-        -((a - d) * np.log(one_plus_X_over_c_b) / one_plus_X_over_c_b**g)
-        * sample_weight
+        -(a - d) * g * X_div_c_b * np.where(X > 0, np.log(X_div_c), 0) / denom_g_plus_1
     )
-    return np.column_stack((J_a, J_b, J_c, J_d, J_g))
+
+    # Derivative with respect to c
+    J_c = (a - d) * g * b * X**b / (c ** (b + 1) * denom_g_plus_1)
+
+    # Derivative with respect to d
+    J_d = 1 - J_a
+
+    # Derivative with respect to g
+    J_g = -(a - d) * np.log(1 + X_div_c_b) / denom
+
+    # Stack the derivatives and apply sample weights
+    J = np.column_stack((J_a, J_b, J_c, J_d, J_g))
+    return J * sample_weight[:, np.newaxis]
 
 
 five_param_logistic_model = Model(